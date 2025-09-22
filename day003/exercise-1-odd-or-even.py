e2b02b761016dcd03fe5855da1e801426d9f700d
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-22 15:22:06 +0000

    day 3

---
+++ day003/exercise-1-odd-or-even.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+#writing the if statement
+if number % 2 == 0:
+    print("This is an even number")
+
+#writing the else statement
+else:
+    print("This is an odd number")
+
