06e2a0f2d11a75a671d7fcef457d80b3287ea192
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-23 07:08:16 +0000

    day 3

--- day003/exercise-2-BMI-calculator.py
+++ day003/exercise-2-BMI-calculator.py
@@ -16,9 +16,9 @@ if bmi < 18.5:
     print(f"Your BMI is {round_bmi}, you are underweight.")
 elif bmi < 25:
     print(f"Your BMI is {round_bmi}, you have a normal weight.")
-elif bmi <= 30:
+elif bmi < 30:
     print(f"Your BMI is {round_bmi}, you are slightly overweight.")
-elif bmi <= 35:
+elif bmi < 35:
     print(f"Your BMI is {round_bmi}, you are obese.")
 else:
     print(f"Your BMI is {round_bmi}, you have clinically obese.")
