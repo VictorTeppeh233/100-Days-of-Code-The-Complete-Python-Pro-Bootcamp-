7010df1cae931f3bc02d7a95698af0154fb4050e
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-30 23:17:12 +0000

    day 5

---
+++ day005/exercise-3-adding-even-numbers.py
@@ -0,0 +1,8 @@
+#Write your code below this row 👇
+# calculate the sum of even numbers from 1 to 100
+
+multiple = 0
+for even_number in range(2, 101, 2):
+    multiple += even_number
+
+print(multiple)
