b344b166882b967acbe9a98e3ee6e0a608c3f3b9
Victor Teppeh <victorteppeh233@gmail.com>
2026-02-08 22:51:15 +0000

    day 1

--- day001/exercise1-printing.py
+++ day001/exercise1-printing.py
@@ -1,10 +1,5 @@
 # Printing some specific text
-# The code below is already written for you.
-# You just need to run it.
-# The correct output is:
-# Day 1 - Python Print Function
-# The function is declared like this:
-# print('what to print')    
+# Write your code below this text
 
 # Printing texts
 print("Day 1 - Python Print Function")
